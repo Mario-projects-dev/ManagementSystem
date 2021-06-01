cd4db4bcea2190c2c7a7cc663c15277aa0a20902
Mario-projects-dev <m.vitek.mv@gmail.com>
2021-06-01 22:29:50 +0200

    create classes

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream.h>
+#include <fstream.h>
+#include <process.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <conio.h>
+#include <dos.h>
+
+
+//class menu: controls over all functions
+
+class menu
+{
+public: void main_menu(void);
+private: void edit_menu(void);
+};
+
+
+class product
+{
+public:
+	void add_item(void);
+
+};
